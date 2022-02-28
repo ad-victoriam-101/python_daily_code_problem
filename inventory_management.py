23d3a8a0e6508398845d71f9d9144e583b7677a3
ad_victoriam_101 <keeper24@gmail.com>
2022-02-28 15:18:14 -0800

    added idk"

---
+++ inventory_management.py
@@ -0,0 +1,11 @@
+def solution(value1, weight1, value2, weight2, maxW):
+    if weight1 + weight2 <= maxW:
+        return value1 + value2
+    else:
+        #max takes two values or an iterable and returns the max value.
+        # in this case, using one liners we can traverse the if else in a more confined box. 
+        
+        return max(value1 if weight1 <= maxW else 0, value2 if weight2 <= maxW else 0)
+
+   
+assert(solution(10,5,6,4,8)==10)
