a01d1f2c0fc09e61b787bd53673485b78cf2ca4d
ad_victoriam_101 <keeper24@gmail.com>
2022-03-01 12:35:42 -0800

    added new py file

---
+++ will_you.py
@@ -0,0 +1,7 @@
+def solution(young, beautiful, loved):
+    if (young and beautiful) and not loved:
+        return True
+    if(loved and (not young or not beautiful)):
+        return True
+    return False
+    
