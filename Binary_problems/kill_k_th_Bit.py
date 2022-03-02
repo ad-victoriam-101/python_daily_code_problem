98519d13ab06c94f0aee630262a67de02992bda5
ad_victoriam_101 <keeper24@gmail.com>
2022-03-01 16:03:13 -0800

    added new comments. and assert

--- Binary_problems/kill_k_th_Bit.py
+++ Binary_problems/kill_k_th_Bit.py
@@ -1,9 +1,10 @@
 def solution(n, k):
     """
     :param n: int to be converted to binary
-    :param k: k-th value of bin(n) to change to a 0 if its a 1.
+    :param k: k-th value of bin(n) to change to a 0 if it's a 1.
     :return: an int value, changed by the kth binary value of n
     """
+    # x<<y returns x with the bits shifted to the left by y places. and that the ~ is a mask.
     return n & ~(1 << k-1)
 
 
