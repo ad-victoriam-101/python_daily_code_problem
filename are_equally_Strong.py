b37b83751770a2f865b34523df68e9445d6d5fc3
ad_victoriam_101 <keeper24@gmail.com>
2022-03-07 18:53:22 -0800

    added new file, with doc string and comments.

--- are_equally_Strong.py
+++ are_equally_Strong.py
@@ -10,6 +10,11 @@ def solution(yourLeft, yourRight, friendsLeft, friendsRight):
     """
     return max(yourLeft, yourRight) == max(friendsLeft, friendsRight) and min(yourLeft, yourRight) == min(friendsLeft,
                                                                                                           friendsRight)
-
+"""
+Another solution is as follows, remember that a {} is considered a set in python and sorts the values. 
+"""
+def solution2(yourLeft, yourRight, friendsLeft, friendsRight):
+    print({yourLeft, yourRight},{friendsLeft,friendsRight})
+    return {yourLeft, yourRight} == {friendsLeft, friendsRight}
 
 assert solution(10, 15, 15, 10) == True
