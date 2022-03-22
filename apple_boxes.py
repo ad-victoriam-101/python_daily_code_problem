26ded009931b49637baafc314c8818f9638341ec
ad_victoriam_101 <keeper24@gmail.com>
2022-03-22 12:05:12 -0700

    added new file, with doc string and comments.

---
+++ apple_boxes.py
@@ -0,0 +1,10 @@
+def solution(k):
+    list_k = [i for i in range(1, k+1)]
+    even_nums = list_k[::2]
+    odd_nums = list_k[1::2]
+    even_sum = sum([i*i for i in even_nums])
+    odd_sum = sum([i*i for i in odd_nums])
+    #print(even_nums, even_sum, odd_nums, odd_sum)
+    return odd_sum-even_sum
+    
+print(solution(5))
