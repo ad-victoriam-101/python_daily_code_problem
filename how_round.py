4bae10d5242c17910bf422fe4e152860a45efb60
Brogan Stich <keeper24@gmail.com>
2022-04-11 18:48:48 -0700

    Update how_round.py
    New function call old function was trash

--- how_round.py
+++ how_round.py
@@ -6,9 +6,15 @@ If it's less than 5 we round it to 0 if it's larger than or equal to 5 we round 
 The process stops immediately once there is only one non-zero digit left.
 """
 
-def solution(n)->int:
-    round_list = [(int(x)) for x in str(n)]
-    
-    return round_list
 
-print(solution(1234))
+def solution(n) -> int:
+    x = 1
+
+    while n > 10:
+        n = (n + 5) // 10
+        x *= 10
+    return x * n
+
+
+if __name__ == '__main__':
+    print(solution(1234))
