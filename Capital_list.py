8a6a567790ace534a2dd52c0ebad1fcb0822420e
Brogan Stich <keeper24@gmail.com>
2022-05-03 16:49:39 -0700

    Added new file for daily code.

--- Capital_list.py
+++ Capital_list.py
@@ -2,4 +2,5 @@ def capital_index(string) -> list:
     return [i for i in range(0, len(string)) if string[i].isupper()]
 
 
-print(capital_index("TEsT"))
+if __name__ == "__main__":
+    assert (capital_index("TEsT")) == [0, 1, 3]
