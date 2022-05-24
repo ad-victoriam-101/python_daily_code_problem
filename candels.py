5c6af639e6ce61d550b561bcaee5798eba90f89d
Brogan Stich <keeper24@gmail.com>
2022-05-23 21:56:04 -0700

    New file added for daily question

---
+++ candels.py
@@ -0,0 +1,15 @@
+"""
+When a candle finishes burning it leaves a leftover. makeNew leftovers can be combined to make a new candle, which,
+when burning down, will in turn leave another leftover.
+
+You have solutionNumber solution in your possession. What's the total number of solution you can burn,
+assuming that you create new solution as soon as you have enough leftovers?
+Example
+
+For solutionNumber = 5 and makeNew = 2, the output should be
+solution(solutionNumber, makeNew) = 9.
+"""
+
+def candel_solution(c_n,new_requirment) => int:
+    pass
+
