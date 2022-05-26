0a672e104e455d5730125ee15690b376164adab7
Brogan Stich <keeper24@gmail.com>
2022-05-26 10:20:15 -0700

    added new file.

--- longestSubstring.py
+++ longestSubstring.py
@@ -4,3 +4,12 @@ Given a string given the longest constant substring that contains at most 2 uniq
 """
 
 def len_of_two_sub_strings(string):
+    """
+
+    Args:
+        string: random string of letters,
+
+    Returns: longest sub set of letters
+
+    """
+    pass
