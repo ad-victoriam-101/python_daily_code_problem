69b5250b1f3db329b91b119fa87a317dc7fe59c5
Brogan Stich <keeper24@gmail.com>
2022-05-26 11:16:51 -0700

    Added proper file format and most of the coding

--- longestSubstring.py
+++ longestSubstring.py
@@ -3,6 +3,7 @@ Given a string given the longest constant substring that contains at most 2 uniq
 
 """
 
+
 def len_of_two_sub_strings(string):
     """
 
@@ -12,4 +13,43 @@ def len_of_two_sub_strings(string):
     Returns: longest sub set of letters
 
     """
-    pass
+    max_char = 0
+    left = 0
+    right = 1
+    def is_two_char(st):
+        return len(set(st))<=2
+
+
+    while (left < right):
+        if is_two_char(string[left:right]) and right < len(string):
+            max_char = max(max_char, len(string[left:right]))
+            right += 1
+        elif is_two_char(string[left:right]) and right == len(string):
+            return max(max_char, len(string[left:right]))
+        else:
+            left += 1
+
+    return max_char
+
+print(len_of_two_sub_strings("ababccde"))
+
+def lengthOfLongestSubstringTwoDistinct(s):
+    maxLen = 0  # this will be returned at the end, and it maintains my current max found
+    # s[right:left], s.substring(0,1)
+    left = 0
+    right = 1
+
+    def isAtMostTwoChars(st):
+        return len(set(st)) <= 2
+
+    while (left < right):  # since I move both flags, left will eventually hit right once right hits the end
+        if isAtMostTwoChars(s[left:right]) and right < len(s):
+            maxLen = max(maxLen, len(s[left:right]))
+            right += 1
+        elif isAtMostTwoChars(s[left:right]) and right == len(s):
+            return max(maxLen, len(s[left:right]))
+        else:
+            left += 1
+
+    return maxLen
+print(lengthOfLongestSubstringTwoDistinct("ababccde"))
