cec91e17b1abef50fbecffbd4eb5aca9f0a8f227
Brogan Stich <keeper24@gmail.com>
2022-06-07 13:10:35 -0700

    Added solution_2 to two_sums_list.py, with hash table intro.

--- two_sums_list.py
+++ two_sums_list.py
@@ -8,12 +8,22 @@ You can return the answer in any order.
 
 
 def solution(nums, target):
-    left = 0
-    right = 1
-    while right < len(nums)-1:
-        print(left,right)
-        right += 1
+    for left in range(len(nums) - 1):
+        right = left + 1
+        while right < len(nums):
+            if nums[left] + nums[right] == target:
+                print([left, right])
+            right += 1
 
 
-print(solution([2, 7, 11, 15], 9))
+def solution_2(nums, target):
+    hash_map = {}
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if complement in hash_map:
+            return [i, hash_map[complement]]
+        hash_map[nums[i]] = i
+
+
+print(solution_2([2, 7, 11, 15], 9))
 print(solution([3, 2, 4], 6))
