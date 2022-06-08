ee0ab692a765a373dc3d68ea60fa89d9c317317e
Brogan Stich <keeper24@gmail.com>
2022-06-08 10:22:22 -0700

    New file, And python code.

---
+++ intersection_of_arrays.py
@@ -0,0 +1,17 @@
+"""
+Given two integer arrays nums1 and nums2,
+return an array of their intersection.
+Each element in the result must appear as many times as it shows in both arrays
+and you may return the result in any order.
+"""
+def solution(nums1,nums2):
+    """
+    Args:
+        nums1: list of ints
+        nums2: list of ints
+
+    Returns:
+        list of each element that appears in each array.
+        Input: nums1 = [1,2,2,1], nums2 = [2,2]
+        Output: [2,2]
+    """
