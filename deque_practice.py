76530b45b89fa0d4fec36edcc91fa5416bcbc274
Brogan Stich <keeper24@gmail.com>
2022-06-28 14:02:54 -0700

    New file, for Deque practice

--- deque_practice.py
+++ deque_practice.py
@@ -1,7 +1,19 @@
 import collections
 
 deque_colors = collections.deque(["red", "green", "white"])
+print(deque_colors)
 #append to the left.
 deque_colors.appendleft("pink")
+print(deque_colors)
 #append to the right
 deque_colors.append("orange")
+print(deque_colors)
+# pip from the right
+deque_colors.pop()
+print(deque_colors)
+# pop from the left
+deque_colors.popleft()
+print(deque_colors)
+# reverse the order
+deque_colors.reverse()
+print(deque_colors)
