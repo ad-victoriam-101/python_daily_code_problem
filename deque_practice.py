ae9924cc12b8278c71126d0a2f1cedf4f28236b7
Brogan Stich <keeper24@gmail.com>
2022-06-28 13:52:14 -0700

    New file, for Deque practice

---
+++ deque_practice.py
@@ -0,0 +1,7 @@
+import collections
+
+deque_colors = collections.deque(["red", "green", "white"])
+#append to the left.
+deque_colors.appendleft("pink")
+#append to the right
+deque_colors.append("orange")
