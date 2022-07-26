62a10ce6efbedf594fe59c8399b8ce5207a97021
Brogan Stich <keeper24@gmail.com>
2022-07-26 11:36:56 -0700

    Added new file and intro boilerplate

---
+++ Roman_To_Int.py
@@ -0,0 +1,15 @@
+# Given a roman numeral return the int value.
+#Start by making a dic of vals
+roman_values = {
+    "I":1,
+    "V":5,
+    "X":10,
+    "L":50,
+    "C":100,
+    "D":500,
+    "M":1000,
+}
+def roman_to_int(str):
+    r_to_i = 0
+    for char in str:
+        pass
