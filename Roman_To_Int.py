2eca661f72147890152f604c11ac7d6c26d22b76
Brogan Stich <keeper24@gmail.com>
2022-07-28 09:17:47 -0700

    Added new file and intro boilerplate

--- Roman_To_Int.py
+++ Roman_To_Int.py
@@ -1,21 +1,25 @@
 # Given a roman numeral return the int value.
-#Start by making a dic of vals
+# Start by making a dic of vals
 roman_values = {
-    "I":1,
-    "V":5,
-    "X":10,
-    "L":50,
-    "C":100,
-    "D":500,
-    "M":1000,
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000,
 }
+
+
 def roman_to_int(str):
     r_to_i = 0
     for index, numeral in enumerate(str):
-        if index<len(s)-1 and roman_values[numeral] < roman_values[str[index + 1]]:
+        if index < len(str) - 1 and roman_values[numeral] < roman_values[str[index + 1]]:
             r_to_i -= roman_values[numeral]
         else:
             r_to_i += roman_values[numeral]
     return r_to_i
 
+
 print(roman_to_int('VI'))
+print(roman_to_int('XXCI'))
