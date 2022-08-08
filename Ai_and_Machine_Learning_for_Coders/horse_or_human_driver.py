e36d48f3185451dde7b7008afc91d3dff6330be9
Brogan Stich <keeper24@gmail.com>
2022-08-08 14:52:16 -0700

    Made new folders for daily's and added folder for new book material.

--- Ai_and_Machine_Learning_for_Coders/horse_or_human_driver.py
+++ Ai_and_Machine_Learning_for_Coders/horse_or_human_driver.py
@@ -1,12 +1,34 @@
+import os.path
 import urllib.request
 import zipfile
 
+# Importing the Training Dataset.
 url = 'https://storage.googleapis.com/laurencemoroney-blog.appspot.com/horse-or-human.zip'
 
 file_name = "horse-or-human.zip"
 training_dir = "horse-or-human/training/"
-urllib.request.urlretrieve(url, file_name)
 
-zip_ref = zipfile.ZipFile(file_name,"r")
-zip_ref.extractall(training_dir)
-zip_ref.close()
+if os.path.exists(training_dir):
+    pass
+else:
+    urllib.request.urlretrieve(url, file_name)
+
+    zip_ref = zipfile.ZipFile(file_name, "r")
+    zip_ref.extractall(training_dir)
+    zip_ref.close()
+
+# Importing the validation dataset.
+
+validation_url = "https://storage.googleapis.com/laurencemoroney-blog.appspot.com/validation-horse-or-human.zip"
+
+validation_file_name = 'validation-horse-or-human.zip'
+validation_dir = 'horse-or-human/validation/'
+
+if os.path.exists(validation_dir):
+    pass
+else:
+    urllib.request.urlretrieve(validation_url, validation_file_name)
+    zip_ref = zipfile.ZipFile(validation_file_name, 'r')
+    zip_ref.extractall(validation_dir)
+    zip_ref.close()
+    print(f'new files added under:{validation_dir + validation_file_name}')
---
+++ Ai_and_Machine_Learning_for_Coders/is_it_a_horse_or_a_human.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+training_dir = "horse-or-human/training/"
+validation_dir = "horse-or-human/validation/"
+
+train_datagen = ImageDataGenerator(rescale=1 / 255)
+validation_datagen = ImageDataGenerator(rescale=1 / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    training_dir,
+    target_size=(300, 300),
+    class_mode='binary',
+)
+
+validation_generator = validation_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(300, 300),
+    class_mode='binary'
+)
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, (3, 3), activation='relu',
+                           input_shape=(300, 300, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid'),
+])
+
+print(model.summary())
+
+model.compile(
+    loss='binary_crossentropy',
+    optimizer=tf.optimizers.RMSprop(lr=0.001),
+    metrics=['accuracy'],
+)
+
+history = model.fit_generator(
+    train_generator,
+    epochs=15,
+    validation_data=validation_generator,
+)
