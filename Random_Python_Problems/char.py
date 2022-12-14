664ca01b452fc53f945c90c4baee637b5c058258
Brogan Stich <keeper24@gmail.com>
2022-12-13 21:41:27 -0800

    Made new folders for daily's and added folder for new book material.

--- Random_Python_Problems/char.py
+++ Random_Python_Problems/char.py
@@ -1,1 +1,14 @@
+char = "Close"
+return_list = []
+val=0
+import numpy
+for i in char:
 
+    val += ord(i)
+    return_list.append(val)
+
+print(return_list)
+
+one_line = numpy.cumsum(10*[ord(i) for i in list("Close")])
+
+print(one_line)
