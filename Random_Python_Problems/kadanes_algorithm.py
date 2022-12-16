08da9cca4817b8fce1891fd8d329c980d72e6569
Brogan Stich <keeper24@gmail.com>
2022-12-15 21:57:20 -0800

    Made new folders for daily's and added folder for new book material.

--- Random_Python_Problems/kadanes_algorithm.py
+++ Random_Python_Problems/kadanes_algorithm.py
@@ -1,6 +1,16 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         # kadane's algorithm
+        '''
+
+        Args:
+            nums: list of ints contains both positive
+            and negative values
+
+        Returns:
+            the maximum sum of possible values.
+
+        '''
         curSum = maxSum = nums[0]
         for num in nums[1:]:
             # python MAX() returns the max between two values
