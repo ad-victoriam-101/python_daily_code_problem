19e6b8da54f6a332d6416d943be6b38b8bd47aaa
Brogan Stich <keeper24@gmail.com>
2022-12-15 21:56:29 -0800

    Made new folders for daily's and added folder for new book material.

--- Random_Python_Problems/kadanes_algorithm.py
+++ Random_Python_Problems/kadanes_algorithm.py
@@ -1,10 +1,9 @@
-
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         # kadane's algorithm
         curSum = maxSum = nums[0]
         for num in nums[1:]:
-            #python MAX() returns the max between two values
+            # python MAX() returns the max between two values
 
             curSum = max(curSum, 0) + num
             maxSum = max(maxSum, curSum)
