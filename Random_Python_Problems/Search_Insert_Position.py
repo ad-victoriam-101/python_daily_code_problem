862046d5630218af0cba8f3dc49d7e30a1699964
broganstich <keeper24@gmail.com>
2023-01-04 15:33:39 -0800

    Added new file and beginning class solution.

---
+++ Random_Python_Problems/Search_Insert_Position.py
@@ -0,0 +1,12 @@
+class Solution:
+    def search_insert(self, nums: List[int], target: int) -> int:
+        """
+        Args:
+            nums: List of distinct integers,
+            target: int: value either within nums or not
+
+        Returns:
+            IF the target is in nums: returns the index of the target.
+            ELSE: returns the index where it would be inserted in order.
+
+        """
