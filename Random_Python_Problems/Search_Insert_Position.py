9b113893a669a240e444aa555098d5e0c20c83e6
broganstich <keeper24@gmail.com>
2023-01-05 09:38:07 -0800

    Added new file and beginning class solution.

--- Random_Python_Problems/Search_Insert_Position.py
+++ Random_Python_Problems/Search_Insert_Position.py
@@ -1,4 +1,21 @@
 import bisect
+
+
+def bisect_search(nums: List[int], target: int) -> int:
+    """
+    Args:
+        nums: List of distinct integers,
+        target: int: value either within nums or not
+    Returns:
+        IF the target is in nums: returns the index of the target.
+        ELSE: returns the index where it would be inserted in order.
+
+    """
+    # Bisect Module
+    # import bisect assumed.
+    return bisect.bisect_left(nums, target)
+
+
 class Solution:
     def search_insert(self, nums: List[int], target: int) -> int:
         """
@@ -18,19 +35,5 @@ class Solution:
             if num >= target:
                 return i
         return len(nums)
-
-    def bisect_search(self, nums: List[int], target: int) -> int:
-        """
-        Args:
-            nums: List of distinct integers,
-            target: int: value either within nums or not
-        Returns:
-            IF the target is in nums: returns the index of the target.
-            ELSE: returns the index where it would be inserted in order.
-
-        """
-        # Bisect Module
-        # import bisect assumed.
-        return bisect.bisect_left(nums, target)
 
 
