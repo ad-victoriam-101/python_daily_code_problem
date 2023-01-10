168d748046144e1cfb759d53c89bba4fc6ee092c
Brogan Stich <keeper24@gmail.com>
2023-01-09 20:42:48 -0800

    added single line solution

--- Random_Python_Problems/single_number.py
+++ Random_Python_Problems/single_number.py
@@ -1,5 +1,11 @@
 """
 in this problem we are asked to return the unique
 single value from a given list of repeating values.
-
 """
+import operator
+from functools import reduce
+
+
+class Solution:
+    def single_number(self, nums):
+        return reduce(operator.xor, nums)
