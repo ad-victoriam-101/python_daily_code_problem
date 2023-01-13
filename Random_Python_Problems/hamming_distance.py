1dc6d030c7a7d1d65b1823bbc74645397d923145
broganstich <keeper24@gmail.com>
2023-01-13 09:54:52 -0800

    Added new file and description.

--- Random_Python_Problems/hamming_distance.py
+++ Random_Python_Problems/hamming_distance.py
@@ -12,6 +12,8 @@ class Solution:
         for i in range(31, -1, -1):
             binary1 = x >> i & 1
             binary2 = y >> i & 1
-            answer += not(binary1-binary2)
+            answer += not (binary1 - binary2)
         return answer
 
+    def hamming_distance_short(self, x: int, y: int) -> int:
+        return bin(x ^ y).count('1')
