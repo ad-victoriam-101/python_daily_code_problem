3bfa73ae443f14654ff206572918e322d3d42f15
broganstich <keeper24@gmail.com>
2023-01-18 09:01:25 -0800

    Added new file and description.

--- Random_Python_Problems/sum_of_1d_array.py
+++ Random_Python_Problems/sum_of_1d_array.py
@@ -3,13 +3,23 @@ Given an array nums. We define a running sum of an array a runningSum[i] = sum(n
 
 Return the running sum of nums.
 """
+import itertools
 
 
 class Solution:
-    def runnin_sum(self, nums: List[int]) -> List[int]:
+    def runnin_sum(self, nums: list[int]) -> list[int]:
         start = 0
         return_list = []
         for i in range(len(nums)):
             return_list.append(nums[i] + start)
             start += nums[i]
         return return_list
+
+    def one_liner(self, nums: list[int]) -> list[int]:
+        return list(itertools.accumulate(nums))
+
+
+solution = Solution()
+solution.runnin_sum([1,2,3,4])
+print(solution.one_liner([1,2,3,4]))
+
