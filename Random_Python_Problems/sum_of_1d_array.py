2aed2d211bd2a51a55146dbaa0fcaf23429562be
broganstich <keeper24@gmail.com>
2023-01-18 08:46:02 -0800

    Added new file and description.

---
+++ Random_Python_Problems/sum_of_1d_array.py
@@ -0,0 +1,7 @@
+"""
+Given an array nums. We define a running sum of an array a runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+"""
+class Solution:
+    def runnin_sum(self,nums : List[int])->List[int]:
