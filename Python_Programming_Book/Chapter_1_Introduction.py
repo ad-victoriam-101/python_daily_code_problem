18fd39ee7f2620fc0b21f00c043acdbd58300ec9
broganstich <keeper24@gmail.com>
2023-01-25 15:31:55 -0800

    new file, Added daily checkin and new methods in class call

--- Python_Programming_Book/Chapter_1_Introduction.py
+++ Python_Programming_Book/Chapter_1_Introduction.py
@@ -1,4 +1,11 @@
 """
 Chaotic Function an introduction
 """
-def main()
+def main():
+    print("This  program illustrates a chaotic function")
+    x = eval(input("enter a number between 0 and 1: "))
+    if x > 1 or x < 0:
+        print("please enter a value between 0 and 1")
+    for _ in range(10):
+        x = 3.9 * x * (1-x)
+        print(x)
